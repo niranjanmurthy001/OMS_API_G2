a5641bf8109cb9b326d3a8b5fe288a470f30897d
Niranjan Murthy <niranjanmurthy@drnds.com>
2019-12-26 21:45:26 +0530

    Added Break mode And idle mobe in employees folder -vijaykumar

---
+++ OrderManagement_Api/Controllers/Employee/BreakModeController.cs
@@ -0,0 +1,138 @@
+﻿using Newtonsoft.Json;
+using OrderManagement_Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OrderManagement_Api.Controllers.Dashboard
+{
+    public class BreakModeController : ApiController
+    {
+        [HttpPost]
+        [ActionName("BindBreakMode")]
+        public IHttpActionResult BindBreakModeType(dynamic data)
+        {
+            if (data == null) return BadRequest("Not Found");
+            try
+            {
+                var value = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(data));
+                var dt = DbExecute.GetMultipleRecordByParam("Sp_Break_Mode", value);
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    return Ok(dt);
+                }
+                return NotFound();
+            }
+            catch (HttpResponseException ex)
+            {
+                return StatusCode(ex.Response.StatusCode);
+            }
+        }
+        [HttpPost]
+        [ActionName("Create")]
+        public IHttpActionResult InsertData(dynamic data)
+        {
+            if (data == null) return BadRequest("Please Provide the Valid Details ");
+            try
+            {
+                var item = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(data));
+                int dt = Convert.ToInt32(DbExecute.ExecuteSPForScalar("Sp_Order_User_Break_Details", item));
+                if (dt > 0)
+                {
+                    return Ok(dt);
+                }
+                return NotFound();
+            }
+            catch (HttpResponseException ex)
+            {
+                return StatusCode(ex.Response.StatusCode);
+            }
+        }
+        [HttpPost]
+        [ActionName("LoadStartTime")]
+        public IHttpActionResult GetStartTime(dynamic data)
+        {
+            if (data == null) return BadRequest("Not Found");
+            try
+            {
+                var value = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(data));
+                var dt = DbExecute.GetMultipleRecordByParam("Sp_Order_User_Break_Details", value);
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    return Ok(dt);
+                }
+                return NotFound();
+            }
+            catch (HttpResponseException ex)
+            {
+                return StatusCode(ex.Response.StatusCode);
+            }
+        }
+
+        [HttpPut]
+        [ActionName("Update")]
+        public IHttpActionResult UpdateEndTime(dynamic data)
+        {
+            if (data == null) return BadRequest("Please Provide the Valid Details ");
+            try
+            {
+                var item = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(data));
+                int dt = Convert.ToInt32(DbExecute.ExecuteSPForCRUD("Sp_Order_User_Break_Details", item));
+                if (dt > 0)
+                {
+                    return Ok(dt);
+                }
+                return NotFound();
+            }
+            catch (HttpResponseException ex)
+            {
+                return StatusCode(ex.Response.StatusCode);
+            }
+        }
+        [HttpPost]
+        [ActionName("LoadTimings")]
+        public IHttpActionResult GetStartEndTimings(dynamic data)
+        {
+            if (data == null) return BadRequest("Not Found");
+            try
+            {
+                var value = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(data));
+                var dt = DbExecute.GetMultipleRecordByParam("Sp_Order_User_Break_Details", value);
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    return Ok(dt);
+                }
+                return NotFound();
+            }
+            catch (HttpResponseException ex)
+            {
+                return StatusCode(ex.Response.StatusCode);
+            }
+        }
+        [HttpPost]
+        [ActionName("Get_BreakDetail")]
+        public IHttpActionResult Get_break_Details(dynamic data)
+        {
+            if (data == null) return BadRequest("Not Found");
+            try
+            {
+                var value = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(data));
+                 var   dt = DbExecute.GetMultipleRecordByParam("Sp_Order_User_Break_Details", value);
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    return Ok(dt);
+                }
+                return NotFound();
+            }
+            catch (HttpResponseException ex)
+            {
+                return StatusCode(ex.Response.StatusCode);
+            }
+        }
+    }
+}
+
+
---
+++ OrderManagement_Api/Controllers/Employee/IdleTrackModeController.cs
@@ -0,0 +1,97 @@
+﻿using Newtonsoft.Json;
+using OrderManagement_Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OrderManagement_Api.Controllers.Dashboard
+{
+    public class IdleTrackModeController : ApiController
+    {
+        [HttpPost]
+        [ActionName("Insert")]
+        public IHttpActionResult InsertTiming(dynamic data)
+        {
+            if (data == null) return BadRequest("Not Found");
+            try
+            {
+                var value = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(data));
+                var dt =Convert.ToInt32(DbExecute.ExecuteSPForScalar("SP_User_Idle_Timings", value));
+                if (dt > 0)
+                {
+                    return Ok(dt);
+                }
+                return NotFound();
+            }
+            catch (HttpResponseException ex)
+            {
+                return StatusCode(ex.Response.StatusCode);
+            }
+        }
+        [HttpPost]
+        [ActionName("Select")]
+        public IHttpActionResult GetUserIdleTimings(dynamic data)
+        {
+            if (data == null) return BadRequest("Not Found");
+            try
+            {
+                var value = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(data));
+                var dt = DbExecute.GetMultipleRecordByParam("SP_User_Idle_Timings", value);
+                if (dt != null)
+                {
+                    return Ok(dt);
+                }
+                return NotFound();
+            }
+            catch (HttpResponseException ex)
+            {
+                return StatusCode(ex.Response.StatusCode);
+            }
+        }
+
+        [HttpPut]
+        [ActionName("Update")]
+        public IHttpActionResult UpdateUserTiming(dynamic data)
+        {
+            if (data == null) return BadRequest("Not Found");
+            try
+            {
+                var value = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(data));
+                var dt = DbExecute.ExecuteSPForCRUD("SP_User_Idle_Timings", value);
+                if (dt != null)
+                {
+                    return Ok(dt);
+                }
+                return NotFound();
+            }
+            catch (HttpResponseException ex)
+            {
+                return StatusCode(ex.Response.StatusCode);
+            }
+        }
+        [HttpPost]
+        [ActionName("LoadTime")]
+        public IHttpActionResult LoadTimerValues(dynamic data)
+        {
+            if (data == null) return BadRequest("Not Found");
+            try
+            {
+                var value = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(data));
+                var dt = DbExecute.GetMultipleRecordByParam("SP_User_Idle_Timings", value);
+                if (dt != null)
+                {
+                    return Ok(dt);
+                }
+                return NotFound();
+            }
+            catch (HttpResponseException ex)
+            {
+                return StatusCode(ex.Response.StatusCode);
+            }
+        }
+    
+    }
+}
