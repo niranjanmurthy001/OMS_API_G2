2fbec372117a1befc9e78fb98ae70c693052175f
Niranjan Murthy <niranjanmurthy@drnds.com>
2020-01-17 03:25:05 +0530

    Changes in OrderHistory Controller-Jeevan kumar

--- OrderManagement_Api/Controllers/Order/OrderHistoryController.cs
+++ OrderManagement_Api/Controllers/Order/OrderHistoryController.cs
@@ -52,5 +52,25 @@ namespace OrderManagement_Api.Controllers.Order
                 return StatusCode(ex.Response.StatusCode);
             }
         }
+        [HttpPost]
+        [ActionName("BindOrderStatusPermission")]
+        public IHttpActionResult BindStatusPermissionHistory(dynamic data)
+        {
+            if (data == null) return BadRequest("Not Found");
+            try
+            {
+                var value = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(data));
+                var dt = DbExecute.GetMultipleRecordByParam("Sp_Order_Status_permission_History", value);
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    return Ok(dt);
+                }
+                return NotFound();
+            }
+            catch (HttpResponseException ex)
+            {
+                return StatusCode(ex.Response.StatusCode);
+            }
+        }
     }
 }
