1636aab99ea5151b387f70206bea63a97c897617
Niranjan Murthy <niranjanmurthy@drnds.com>
2020-01-24 20:06:13 +0530

    Commited Changes in Emailsettings Controller-vijaykumar

---
+++ OrderManagement_Api/Controllers/Order/EmailSetttingsController.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using OrderManagement_Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OrderManagement_Api.Controllers.Order
+{
+    public class EmailSettingsController : ApiController
+    {
+        [HttpPost]
+        [ActionName("BindData")]
+        public IHttpActionResult GetEmailDetails(dynamic data)
+        {
+            if (data == null) return BadRequest("Not Found");
+            try
+            {
+                var value = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(data));
+                var dt = DbExecute.GetMultipleRecordByParam("Sp_EmailVerify", value);
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    return Ok(dt);
+                }
+                return NotFound();
+            }
+            catch (HttpResponseException ex)
+            {
+                return StatusCode(ex.Response.StatusCode);
+            }
+        }
+
+
+    }
+}
