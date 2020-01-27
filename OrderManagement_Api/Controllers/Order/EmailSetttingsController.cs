ffce2a19333d31fa517fedd5e40ce30d429bbf09
Niranjan Murthy <niranjanmurthy@drnds.com>
2020-01-27 17:55:25 +0530

    chnages made in Email Settings Controller-vijay

--- OrderManagement_Api/Controllers/Order/EmailSetttingsController.cs
+++ OrderManagement_Api/Controllers/Order/EmailSetttingsController.cs
@@ -32,6 +32,107 @@ namespace OrderManagement_Api.Controllers.Order
             }
         }
 
+        [HttpPost]
+        [ActionName("Insert")]
+        public IHttpActionResult EmailInsert(dynamic data)
+        {
+            if (data == null) return BadRequest("Not Found");
+            try
+            {
+                var value = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(data));
+                var dt = Convert.ToInt32(DbExecute.ExecuteSPForScalar("Sp_EmailVerify", value));
+                if (dt > 0)
+                {
+                    return Ok(dt);
+                }
+                return NotFound();
+            }
+            catch (HttpResponseException ex)
+            {
+                return StatusCode(ex.Response.StatusCode);
+            }
+        }
 
+        [HttpPut]
+        [ActionName("Update")]
+        public IHttpActionResult EmailUpdate(dynamic data)
+        {
+            if (data == null) return BadRequest("Not Found");
+            try
+            {
+                var value = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(data));
+                var dt = Convert.ToInt32(DbExecute.ExecuteSPForScalar("Sp_EmailVerify", value));
+                if (dt != null )
+                {
+                    return Ok(dt);
+                }
+                return NotFound();
+            }
+            catch (HttpResponseException ex)
+            {
+                return StatusCode(ex.Response.StatusCode);
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Check")]
+        public IHttpActionResult CheckEmailDetails(dynamic data)
+        {
+            if (data == null) return BadRequest("Not Found");
+            try
+            {
+                var value = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(data));
+                var dt = DbExecute.GetMultipleRecordByParam("Sp_EmailVerify", value);
+                if (dt != null)
+                {
+                    return Ok(dt);
+                }
+                return NotFound();
+            }
+            catch (HttpResponseException ex)
+            {
+                return StatusCode(ex.Response.StatusCode);
+            }
+        }
+        [HttpPost]
+        [ActionName("Select")]
+        public IHttpActionResult GetEmailRecords(dynamic data)
+        {
+            if (data == null) return BadRequest("Not Found");
+            try
+            {
+                var value = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(data));
+                var dt = DbExecute.GetMultipleRecordByParam("Sp_EmailVerify", value);
+                if (dt != null)
+                {
+                    return Ok(dt);
+                }
+                return NotFound();
+            }
+            catch (HttpResponseException ex)
+            {
+                return StatusCode(ex.Response.StatusCode);
+            }
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public IHttpActionResult DeleteSelectedEmail(dynamic data)
+        {
+            if (data == null) return BadRequest("Not Found");
+            try
+            {
+                var value = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(data));
+                var dt = DbExecute.GetMultipleRecordByParam("Sp_EmailVerify", value);
+                if (dt !=null)
+                {
+                    return Ok(dt);
+                }
+                return NotFound();
+            }
+            catch (HttpResponseException ex)
+            {
+                return StatusCode(ex.Response.StatusCode);
+            }
+        }
     }
 }
