3fc377e41dbcf48cec84ba6319a298e2282c2baa
Niranjan Murthy <niranjanmurthy@drnds.com>
2020-01-31 03:05:35 +0530

    changes-Jeevan kumar

--- OrderManagement_Api/Controllers/Masters/Client_ProcessController.cs
+++ OrderManagement_Api/Controllers/Masters/Client_ProcessController.cs
@@ -2,12 +2,15 @@
 using OrderManagement_Api.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Web.Http;
 
 namespace OrderManagement_Api.Controllers.Masters
 {
     public class Client_ProcessController : ApiController
     {
+        SqlConnection con;
         [HttpPost]
         [ActionName("BindData")]
         public IHttpActionResult GetClientData(dynamic data)
@@ -36,13 +39,24 @@ namespace OrderManagement_Api.Controllers.Masters
             if (data == null) return BadRequest("Not Found");
             try
             {
-                var value = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(data));
-                var dt = Convert.ToInt32(DbExecute.ExecuteSPForCRUD("usp_Master_Client_Process", value));
-                if (dt > 0)
+                var value = JsonConvert.DeserializeObject<DataTable>(JsonConvert.SerializeObject(data));
+                using (con = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings[1].ConnectionString))
                 {
-                    return Ok(dt);
+                    con.Open();
+                    using (SqlBulkCopy sqlbulk = new SqlBulkCopy(con))
+                    {
+                        sqlbulk.ColumnMappings.Add("Client", "Client");
+                        sqlbulk.ColumnMappings.Add("Sub_Client", "Sub_Client");
+                        sqlbulk.ColumnMappings.Add("Project_Type", "Project_Type");
+                        sqlbulk.ColumnMappings.Add("Department_Type", "Department_Type");
+                        sqlbulk.BulkCopyTimeout = 3000;
+                        sqlbulk.BatchSize = 10000;
+                        sqlbulk.DestinationTableName = "Tbl_Process_Settings";
+                        sqlbulk.WriteToServer(value);
+                        return Ok(value);
+                    }
                 }
-                return NotFound();
+                
             }
             catch (HttpResponseException ex)
             {
