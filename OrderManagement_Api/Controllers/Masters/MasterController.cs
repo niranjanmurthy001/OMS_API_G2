4a1faa6946bbab66fd07d46a83638f3fcb420325
Niranjan Murthy <niranjanmurthy@drnds.com>
2020-02-01 02:10:53 +0530

    Controllers Combined  together related to Process Settings-Jeevan Kumar

---
+++ OrderManagement_Api/Controllers/Masters/MasterController.cs
@@ -0,0 +1,167 @@
+﻿using Newtonsoft.Json;
+using OrderManagement_Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Http;
+
+namespace OrderManagement_Api.Controllers.Masters
+{
+    public class MasterController : ApiController
+    {
+        SqlConnection con;
+        [HttpPost]
+        [ActionName("BindClients")]
+        public IHttpActionResult BindClients(dynamic data)
+        {
+            if (data == null) return BadRequest("Not Found");
+            try
+            {
+                var value = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(data));
+                var dt = DbExecute.GetMultipleRecordByParam("usp_Master_Client_Process", value);
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    return Ok(dt);
+                }
+                return NotFound();
+            }
+            catch (HttpResponseException ex)
+            {
+                return StatusCode(ex.Response.StatusCode);
+            }
+        }
+        [HttpPost]
+        [ActionName("BindSubClients")]
+        public IHttpActionResult SubClients(dynamic data)
+        {
+            if (data == null) return BadRequest("Not Found");
+            try
+            {
+                var value = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(data));
+                var dt = DbExecute.GetMultipleRecordByParam("usp_Master_Client_Process", value);
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    return Ok(dt);
+                }
+                return NotFound();
+            }
+            catch (HttpResponseException ex)
+            {
+                return StatusCode(ex.Response.StatusCode);
+            }
+        }
+        [HttpPost]
+        [ActionName("BindProjectType")]
+        public IHttpActionResult ProjectType(dynamic data)
+        {
+            if (data == null) return BadRequest("Not Found");
+            try
+            {
+                var value = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(data));
+                var dt = DbExecute.GetMultipleRecordByParam("usp_Master_Client_Process", value);
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    return Ok(dt);
+                }
+                return NotFound();
+            }
+            catch (HttpResponseException ex)
+            {
+                return StatusCode(ex.Response.StatusCode);
+            }
+        }
+        [HttpPost]
+        [ActionName("BindDeptType")]
+        public IHttpActionResult Departmenttype(dynamic data)
+        {
+            if (data == null) return BadRequest("Not Found");
+            try
+            {
+                var value = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(data));
+                var dt = DbExecute.GetMultipleRecordByParam("usp_Master_Client_Process", value);
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    return Ok(dt);
+                }
+                return NotFound();
+            }
+            catch (HttpResponseException ex)
+            {
+                return StatusCode(ex.Response.StatusCode);
+            }
+        }        
+        [HttpPost]
+        [ActionName("BindData")]
+        public IHttpActionResult GetClientData(dynamic data)
+        {
+            if (data == null) return BadRequest("Not Found");
+            try
+            {
+                var value = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(data));
+                var dt = DbExecute.GetMultipleRecordByParam("usp_Master_Client_Process", value);
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    return Ok(dt);
+                }
+                return NotFound();
+            }
+            catch (HttpResponseException ex)
+            {
+                return StatusCode(ex.Response.StatusCode);
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Insert")]
+        public IHttpActionResult ClientInsert(dynamic data)
+        {
+            if (data == null) return BadRequest("Not Found");
+            try
+            {
+                var value = JsonConvert.DeserializeObject<DataTable>(JsonConvert.SerializeObject(data));
+                using (con = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings[1].ConnectionString))
+                {
+                    con.Open();
+                    using (SqlBulkCopy sqlbulk = new SqlBulkCopy(con))
+                    {
+                        sqlbulk.ColumnMappings.Add("Client", "Client");
+                        sqlbulk.ColumnMappings.Add("Sub_Client", "Sub_Client");
+                        sqlbulk.ColumnMappings.Add("Project_Type", "Project_Type");
+                        sqlbulk.ColumnMappings.Add("Department_Type", "Department_Type");
+                        sqlbulk.BulkCopyTimeout = 3000;
+                        sqlbulk.BatchSize = 10000;
+                        sqlbulk.DestinationTableName = "Tbl_Process_Settings";
+                        sqlbulk.WriteToServer(value);
+                        return Ok(value);
+                    }
+                }
+
+            }
+            catch (HttpResponseException ex)
+            {
+                return StatusCode(ex.Response.StatusCode);
+            }
+        }
+        [HttpPost]
+        [ActionName("Update")]
+        public IHttpActionResult ClientUpdate(dynamic data)
+        {
+            if (data == null) return BadRequest("Not Found");
+            try
+            {
+                var value = JsonConvert.DeserializeObject<Dictionary<string, object>>(JsonConvert.SerializeObject(data));
+                var dt = Convert.ToInt32(DbExecute.ExecuteSPForCRUD("usp_Master_Client_Process", value));
+                if (dt != null)
+                {
+                    return Ok(dt);
+                }
+                return NotFound();
+            }
+            catch (HttpResponseException ex)
+            {
+                return StatusCode(ex.Response.StatusCode);
+            }
+        }
+    }
+}
